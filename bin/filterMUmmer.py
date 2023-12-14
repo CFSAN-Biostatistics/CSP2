d577704e5e242abff7a8fbfc1baf3ed871bb9b93
Robert Literman <jazz710@gmail.com>
2023-12-14 12:52:05 -0500

    Check up to 10K snps

--- bin/filterMUmmer.py
+++ bin/filterMUmmer.py
@@ -402,7 +402,7 @@ else:
     
     raw_snp_count = count_lines(f"{mum_snps_dir}/{report_id}.snps")
     
-    if raw_snp_count > 5000:
+    if raw_snp_count > 10000:
         sample_category = "Purged_Filter_SNP_Count"
         percent_ref_aligned_filtered = percent_ref_aligned
         percent_query_aligned_filtered = percent_query_aligned
