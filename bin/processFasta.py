783b3d19d3c24b8390189a0b0149d56e3c18117c
Robert Literman <jazz710@gmail.com>
2024-02-29 01:13:49 -0500

    Update processFasta, dev pipelines

--- bin/processFasta.py
+++ bin/processFasta.py
@@ -7,11 +7,31 @@ import os
 
 def fasta_info(file_path):
     records = list(SeqIO.parse(file_path, 'fasta'))
-    contig_count = len(records)
-    assembly_bases = sum(len(record) for record in records)
+    contig_count = int(len(records))
+    lengths = sorted([len(record) for record in records], reverse=True)
+    assembly_bases = sum(lengths)
+
     with open(file_path, 'rb') as file:
         sha256 = hashlib.sha256(file.read()).hexdigest()
-    return contig_count, assembly_bases, sha256
+
+    cumulative_length = 0
+    n50 = None
+    n90 = None
+    l50 = None
+    l90 = None
+    
+    for i, length in enumerate(lengths, start=1):
+        cumulative_length += length
+        if cumulative_length >= assembly_bases * 0.5 and n50 is None:
+            n50 = length
+            l50 = i
+        if cumulative_length >= assembly_bases * 0.9 and n90 is None:
+            n90 = length
+            l90 = i
+        if n50 is not None and n90 is not None:
+            break
+
+    return int(contig_count), int(assembly_bases), int(n50),int(l50), int(n90),int(l90), sha256
 
 sample_name = str(sys.argv[1])
 read_type = str(sys.argv[2])
@@ -23,7 +43,7 @@ if not os.path.exists(fasta_file):
 elif not fasta_file.lower().endswith(('.fa', '.fasta', '.fna')):
     sys.exit(f"File {fasta_file} is not a .fa, .fasta, or .fna file.")
 
-contig_count, assembly_bases, sha256 = fasta_info(fasta_file)
+contig_count, assembly_bases, n50, l50, n90, l90, sha256 = fasta_info(fasta_file)
 
 
-print(f"{sample_name},{read_type},{read_location},{fasta_file},{contig_count},{assembly_bases},{sha256}")
+print(f"{sample_name},{read_type},{read_location},{fasta_file},{contig_count},{assembly_bases},{n50},{l50},{n90},{l90},{sha256}")
